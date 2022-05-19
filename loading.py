Perplexity computation. Fix loss calculations.

loading.py
@@ -1,5 +1,4 @@
 import json
-import os
 from typing import List
 from tqdm import tqdm
 import torch
@@ -8,16 +7,6 @@ from transformers import GPT2TokenizerFast
 from math_tokenize import tokenize_formula, EMPTY_POS_VECTOR, EMPTY_POS_ENCODING
 from constants import Article, TokenType, Sequence, CollatedBatch, PADDING_TOKEN_ID, EOS_TOKEN, FORMULA_IDENTIFIER
 from utils import device
-
-def load_articles():
-    articles: List[Article] = []
-    print("Loading articles...")
-    for article_filename in tqdm(os.listdir("data")):
-        with open(os.path.join("data", article_filename)) as article_file:
-            article = json.load(article_file)
-            article["name"] = article_filename
-            articles.append(article)
-    return articles
 
 
 def split_sequence(sequence: Sequence, max_seq_len: int) -> List[Sequence]:
@@ -57,13 +46,15 @@ def split_sequence(sequence: Sequence, max_seq_len: int) -> List[Sequence]:
 
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, articles: List[Article], max_seq_len: int):
+    def __init__(self, article_filenames: List[str], max_seq_len: int, do_splits: bool = True):
         self.data: List[Sequence] = []
         self.text_tokenizer: GPT2TokenizerFast = GPT2TokenizerFast.from_pretrained("gpt2")
 
         print("Processing data...")
-        for article in tqdm(articles):
-            sequence = Sequence(article["name"])
+        for article_name in tqdm(article_filenames):
+            with open(article_name) as article_file:
+                article: Article = json.load(article_file)
+            sequence = Sequence(article_name)
 
             # Split article text in between formulas
             article_text = article["text"] + EOS_TOKEN
@@ -107,9 +98,11 @@ class Dataset(torch.utils.data.Dataset):
                 sequence.pos_levels.append(0)
                 sequence.pos_encodings.append(EMPTY_POS_ENCODING)
 
-            # Split sequence according to max length and add to final data
-            split_sequences = split_sequence(sequence, max_seq_len)
-            self.data += split_sequences
+            if do_splits:
+                split_sequences = split_sequence(sequence, max_seq_len)
+                self.data += split_sequences
+            else:
+                self.data.append(sequence)
 
     def __len__(self):
         return len(self.data)
@@ -130,6 +123,7 @@ def trim_batch(batch: CollatedBatch, trim_start: int, trim_end: int) -> Collated
         "pos_levels": batch["pos_levels"][:, trim_start : trim_end],
         "pos_encodings": batch["pos_encodings"][:, trim_start : trim_end],
         "attention_mask": batch["attention_mask"][:, trim_start : trim_end],
+        "sequence_lengths": batch["sequence_lengths"], # TODO: do actual calculation, if it matters
     }
 
 
@@ -144,6 +138,7 @@ class Collator:
         pos_level_batches = []
         pos_encoding_batches = []
         attention_mask = []
+        sequence_lengths = []
 
         for sequence in batch:
             token_id_batches.append(torch.LongTensor(sequence.token_ids))
@@ -152,13 +147,16 @@ class Collator:
             pos_level_batches.append(torch.LongTensor(sequence.pos_levels))
             pos_encoding_batches.append(torch.FloatTensor(sequence.pos_encodings))
             attention_mask.append(torch.ones(len(sequence.token_ids)))
+            sequence_lengths.append(len(sequence.token_ids))
 
         return {
             "articles": [sequence.src_article for sequence in batch],
+            # The padding values for token ID and type are critical to correct loss computations in the model
             "token_ids": torch.nn.utils.rnn.pad_sequence(token_id_batches, batch_first=True, padding_value=PADDING_TOKEN_ID).to(device),
-            "token_types": torch.nn.utils.rnn.pad_sequence(token_type_batches, batch_first=True).to(device),
+            "token_types": torch.nn.utils.rnn.pad_sequence(token_type_batches, batch_first=True, padding_value=TokenType.TEXT.value).to(device),
             "pos_vecs": torch.nn.utils.rnn.pad_sequence(pos_vec_batches, batch_first=True).to(device),
             "pos_levels": torch.nn.utils.rnn.pad_sequence(pos_level_batches, batch_first=True).to(device),
             "pos_encodings": torch.nn.utils.rnn.pad_sequence(pos_encoding_batches, batch_first=True).to(device),
             "attention_mask": torch.nn.utils.rnn.pad_sequence(attention_mask, batch_first=True).to(device),
+            "sequence_lengths": sequence_lengths,
         }

model_math_gpt.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 import torch
 from torch import nn
 from transformers import GPT2Model, GPT2LMHeadModel
@@ -176,29 +176,37 @@ class MathGPT(nn.Module):
             type_to_token_probs[token_type][token_idxs] = token_probs * type_probs[:, :, token_type][token_idxs].unsqueeze(1)
         return type_to_token_probs
 
-    def get_prediction_loss(self, type_to_token_probs: Dict[TokenType, torch.Tensor], type_idxs: Dict[TokenType, torch.Tensor], batch: CollatedBatch):
+    def get_prediction_loss(self, type_to_token_probs: Dict[TokenType, torch.Tensor], type_idxs: Dict[TokenType, torch.Tensor],
+                            batch: CollatedBatch, labels: Optional[torch.Tensor]):
         """
         Calculate the cross-entropy prediction loss given the token probabilities
         """
-        # TODO: see if we have to do anything with padding regions
         # TODO: look at numGPT and FORTE papers to verify their token probability calculations
-        loss = torch.Tensor([0]).to(device)
-        shifted_target_tokens = batch["token_ids"][:, 1:]
+        losses: List[torch.Tensor] = []
+        shifted_target_tokens = (batch["token_ids"] if labels is None else labels)[:, 1:]
         for token_type in TokenType:
             # Get indices that have a target of this type
             shifted_type_idx = type_idxs[token_type][:, 1:]
+            # Exclude padding regions, including them would add extra length to the vector that we get the mean of
+            shifted_type_idx &= shifted_target_tokens != PADDING_TOKEN_ID
             # Skip type if it doesn't exist as a target, going through with calculations results in nan loss
             if not torch.any(shifted_type_idx):
                 continue
             # Get token probabilities for this type where the target matches
+            # TODO: problematic articles, token_type=Text for all - List_of_Dallas_Mavericks_seasons, (1,5-anhydro-D-fructose_dehydratase or Field_of_fractions), Geometric_dimensioning_and_tolerancing, Photoconductive_atomic_force_microscopy
             selected_probs = type_to_token_probs[token_type][:, :-1][shifted_type_idx]
-            # Add cross-entropy loss
-            # TODO: seems like we can sometimes get invalid values here, maybe somehow assigning 0 probability to target
+            if 0 in selected_probs:
+                with open("debug.txt", "a") as debug_file:
+                    print("0 target prob detected!")
+                    debug_file.write(f"{batch}\n\n{token_type}\n\n{selected_probs}\n\n{type_to_token_probs}\n\n{type_idxs}\n\n")
+                    continue
+            # Add cross-entropy loss, take average at end to weigh each token equally
             log_probs = torch.log(selected_probs)
-            loss += nn.NLLLoss(ignore_index=PADDING_TOKEN_ID)(log_probs, shifted_target_tokens[shifted_type_idx])
-        return loss
+            loss_fn = nn.NLLLoss(reduction="none")
+            losses.append(loss_fn(log_probs, shifted_target_tokens[shifted_type_idx]))
+        return torch.concat(losses, dim=0).mean()
 
-    def forward(self, batch: CollatedBatch): # TODO: return type
+    def forward(self, batch: CollatedBatch, labels: torch.Tensor = None): # TODO: return type
         # Run inputs through model
         gpt_output: GPTOutput = self.transformer(
             # past_key_values=[], # TODO: for speeding up decoding
@@ -229,7 +237,7 @@ class MathGPT(nn.Module):
         type_to_token_probs = self.get_token_probs(gpt_output, type_probs, type_idxs, final_formula_token_idx)
 
         # Calculate cross-entropy loss
-        loss = self.get_prediction_loss(type_to_token_probs, type_idxs, batch)
+        loss = self.get_prediction_loss(type_to_token_probs, type_idxs, batch, labels)
 
         # Calculate most likely predictions
         predicted_types, predicted_tokens = get_collapsed_predictions(type_to_token_probs)

training.py
@@ -1,17 +1,21 @@
 import time
+import os
 import json
 import torch
 from torch.utils.data import DataLoader, Subset
 import numpy as np
 from sklearn import metrics
 from tqdm import tqdm
-from typing import Optional
+from typing import Optional, List
 
 from model_math_gpt import MathGPT
-from loading import load_articles, Dataset, Collator, trim_batch
+from loading import Dataset, Collator, trim_batch
 from generate import generate, decode_batch
 from utils import TrainOptions, device
-from constants import Mode
+from constants import Mode, PADDING_TOKEN_ID
+
+def get_article_names():
+    return [os.path.join("data", article_filename) for article_filename in os.listdir("data")]
 
 def save_model(model: MathGPT, model_name: str, options: TrainOptions):
     torch.save(model.state_dict(), f"{model_name}.pt")
@@ -98,7 +102,6 @@ def train(model: MathGPT, mode: Mode, model_name: str, train_loader: DataLoader,
             best_stats = cur_stats
             print("Saving model")
             # TODO: consider saving info for checkpointing - epoch, seed, etc. (look up examples)
-            # TODO: saving is slow - see if we can just save best model in memory and then save to file later
             save_model(model, model_name, options)
 
         # Stop training if we haven't improved in a while
@@ -109,7 +112,7 @@ def train(model: MathGPT, mode: Mode, model_name: str, train_loader: DataLoader,
     return best_stats
 
 def pretrain(model_name: str, options: TrainOptions):
-    articles = load_articles()
+    articles = get_article_names()
     dataset = Dataset(articles, options.max_seq_len)
     train_data = Subset(dataset, list(range(0, int(len(dataset) * .9))))
     val_data = Subset(dataset, list(range(int(len(dataset) * .9), len(dataset))))
@@ -129,13 +132,53 @@ def pretrain(model_name: str, options: TrainOptions):
     model = MathGPT().to(device)
     train(model, Mode.PRETRAIN, model_name, train_loader, validation_loader, options)
 
+def evaluate_lm(model_name: str, test_options: dict):
+    model, options = load_model(model_name)
+    options.update(test_options)
+
+    # TODO: definitely need a unit/integration test to cover all the particulars
+    # TODO: get test split from a file
+    # TODO: try excluding test articles that contain UNKs, see how many are left out
+    articles = get_article_names()
+    test_articles = articles[int(len(articles) * .999):]
+    dataset = Dataset(test_articles, options.max_seq_len, do_splits=False)
+    test_loader = DataLoader(
+        dataset,
+        collate_fn=Collator(),
+        batch_size=1, # Only 1 sequence can be processed at a time to recover NLL from the cross-entropy loss (because of padding complications)
+    )
+    # Calculate perplexity - https://huggingface.co/docs/transformers/perplexity
+    # Perplexity = e ^ ((1/n) * nll)
+    print("Calculating perplexity...")
+    with torch.no_grad():
+        nlls: List[torch.Tensor] = []
+        total_sequence_length = 0
+        for batch in tqdm(test_loader):
+            sequence_length = batch["token_ids"].shape[1]
+            total_sequence_length += sequence_length
+            stride = options.stride or options.max_seq_len
+
+            # Get the sum of the NLL for each token in the sequence, using the stride method
+            # Region to left of split point is just for context with no NLL computed, and region to the right contribues to running NLL
+            for split_point in range(0, sequence_length, stride):
+                start_idx = max(split_point + stride - options.max_seq_len, 0)
+                end_idx = min(split_point + stride, sequence_length)
+                target_len = end_idx - split_point # This is equal to stride length except maybe shorter for the last iteration
+                sub_seq_batch = trim_batch(batch, start_idx, end_idx)
+                # Set targets to left of split point to padding so their NLL is not computed
+                labels = torch.clone(sub_seq_batch["token_ids"])
+                labels[:, :-target_len] = PADDING_TOKEN_ID
+                # Run model on batch sub-sequence with altered targets
+                loss = model(sub_seq_batch, labels=labels)[0]
+                # Loss is average NLL over all tokens in the sequence, multiply by number of targets to undo average and retrieve sum
+                nlls.append(loss * target_len)
+        perplexity = torch.exp(torch.sum(torch.stack(nlls)) / total_sequence_length)
+        print("Perplexity", perplexity)
+
 def test_lm(model_name: str, test_article: str):
     model, options = load_model(model_name)
 
-    with open(test_article) as test_article_file:
-        article = json.load(test_article_file)
-        article["name"] = test_article_file
-    dataset = Dataset([article], options.max_seq_len)
+    dataset = Dataset([test_article], options.max_seq_len)
     data_loader = DataLoader(
         dataset,
         collate_fn=Collator(),